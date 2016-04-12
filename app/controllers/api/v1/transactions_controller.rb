2f7d7d4e5c1a9d40effb3801bf6517b3b4c77f30
Adam Hundley <adamhundley@gmail.com>
2016-04-12 17:29:04 -0600

    create transaction::findercontroller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -10,27 +10,6 @@ module Api
       def show
         respond_with Transaction.find(params[:id])
       end
-
-      def find
-        respond_with Finder.find(controller, transaction_params)
-      end
-
-      def find_all
-        respond_with Finder.find_all(controller, transaction_params)
-      end
-
-      def random
-        respond_with Finder.random(controller)
-      end
-
-      private
-        def transaction_params
-          params.permit(:id, :name, :credit_card_number, :invoice_id, :result, :created_at, :updated_at)
-        end
-
-        def controller
-          params[:controller]
-        end
     end
   end
 end
