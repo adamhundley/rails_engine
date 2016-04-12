9cf9a3062699a22a67ff5b0985b6d4aca2e6b32d
Adam Hundley <adamhundley@gmail.com>
2016-04-11 18:49:14 -0600

    Add find by route
    * added the ability for searching my merchant name or id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      get 'merchants/find', to: 'merchants#find'
+      resources :merchants, only: [:index, :show] do
+      end
     end
   end
 end
--- spec/requests/merchants_spec.rb
+++ spec/requests/merchants_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe "Merchants", type: :request do
     end
 
     it "has 200 response code" do
-      get '/api/v1/merchants.json'
+      get '/api/v1/merchants'
       expect(response).to have_http_status(200)
     end
 
     it "renders json" do
-      get '/api/v1/merchants.json'
+      get '/api/v1/merchants'
       expect(response.content_type).to eq("application/json")
     end
 
     it "returns information on a merchant" do
-      get '/api/v1/merchants.json'
+      get '/api/v1/merchants'
 
       merchants = json_body
       parsed_merchant = merchants.first
@@ -27,28 +27,66 @@ RSpec.describe "Merchants", type: :request do
     end
   end
 
-  describe "GET /api/v1/merchant/:id" do
+  describe "GET /api/v1/merchants/:id" do
     before(:each) do
       @merchant = create(:merchant)
     end
 
     it "has 200 response code" do
-      get '/api/v1/merchants/1.json'
+      get '/api/v1/merchants/1'
       expect(response).to have_http_status(200)
     end
 
+    # it "has 404 response code for unknown merchant" do
+    #   get '/api/v1/merchants/12'
+    #   expect(response).to have_http_status(404)
+    # end
+
     it "renders json" do
-      get '/api/v1/merchants/1.json'
+      get '/api/v1/merchants/1'
       expect(response.content_type).to eq("application/json")
     end
 
     it "returns information on a merchant" do
-      get '/api/v1/merchants/1.json'
+      get '/api/v1/merchants/1'
 
       merchant = json_body
 
       expect(merchant.keys). to eq [:id, :name, :created_at, :updated_at]
       expect(merchant[:name]).to eq(@merchant[:name])
+    end
+  end
+
+  describe "GET /api/v1/merchants/find?id=1" do
+    before(:each) do
+      @merchant = create(:merchant)
+    end
+
+    it "has 200 response code" do
+      get '/api/v1/merchants/find?id=1'
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders json" do
+      get '/api/v1/merchants/find?id=1'
+      expect(response.content_type).to eq("application/json")
+    end
+
+    it "returns information on a merchant by name" do
+      get '/api/v1/merchants/find?name=Test Merchant'
+
+      merchant = json_body
+
+      expect(merchant.keys). to eq [:id, :name, :created_at, :updated_at]
+      expect(merchant[:name]).to eq(@merchant[:name])
+    end
+
+    it "returns information on a merchant by id" do
+      get '/api/v1/merchants/find?id=1'
+
+      merchant = json_body
+
+      expect(merchant[:id]).to eq(@merchant[:id])
     end
   end
 end
